45e022b0663219689ea55f6ca875ffa724183e1c
gamer201760 <azamat20170@yandex.ru>
2023-04-19 15:05:05 +0500

    add computePID

--- satelite-oritation.ino
+++ satelite-oritation.ino
@@ -3,10 +3,10 @@
 
 using namespace IntroSatLib;
 
-LightSensor light1(Wire, 0x50);
-LightSensor light2(Wire, 0x51);
-LightSensor light3(Wire, 0x52);
-LightSensor light4(Wire, 0x53);
+LightSensor light1(Wire, 0x51);
+LightSensor light2(Wire, 0x53);
+LightSensor light3(Wire, 0x50);
+LightSensor light4(Wire, 0x52);
 
 void setup() {
   Serial.begin(115200, SERIAL_8E1); // Включения Serial для вывода данных
@@ -29,4 +29,15 @@ void loop() {
   Serial.println();
 
   delay(100);
+}
+
+
+// (вход, установка, п, и, д, период в секундах, мин.выход, макс. выход)
+int computePID(float input, float setpoint, float kp, float ki, float kd, float dt, int minOut, int maxOut) {
+  float err = setpoint - input;
+  static float integral = 0, prevErr = 0;
+  integral = constrain(integral + (float)err * dt * ki, minOut, maxOut);
+  float D = (err - prevErr) / dt;
+  prevErr = err;
+  return constrain(err * kp + integral + D * kd, minOut, maxOut);
 }
